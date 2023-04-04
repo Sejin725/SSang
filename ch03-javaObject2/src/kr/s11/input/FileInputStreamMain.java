c925f70f226ff05c2d2a09807a3d749abfd5c827
Gam <Gam@DESKTOP-DPDCEU9>
2023-04-04 17:48:05 +0900

    end

---
+++ ch03-javaObject2/src/kr/s11/input/FileInputStreamMain.java
@@ -0,0 +1,29 @@
+package kr.s11.input;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamMain {
+	public static void main(String[] args) {
+		FileInputStream fis = null;
+		int readbyte;
+		try {
+			//파일로부터 데이터를 읽어오기 위해
+			//FileInputStream 객체를 생성
+			fis = new FileInputStream("file.txt");
+			//파일의 정보를 한 문자씩 읽어들여 아스키 코드로 반환
+			//영문 이외의 문자는 꺠짐
+			while ((readbyte=fis.read()) != -1) {
+				System.out.print((char)readbyte);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  finally {
+			//자원정리
+			if (fis != null) try {fis.close();} catch(IOException e) {}
+		}
+	}
+}
--- ch03-javaObject2/src/kr/s11/input/StandardInputMain01.java
+++ ch03-javaObject2/src/kr/s11/input/StandardInputMain01.java
@@ -1,5 +1,30 @@
 package kr.s11.input;
 
-public class StandardInputMain01 {
+import java.io.IOException;
 
+public class StandardInputMain01 {
+	public static void main(String[] args) {
+		//자바의 기본 입력
+		try {
+			System.out.print("영문자 1개 입력 : ");
+			//문자 하나를 입력 받아서 아스키 코드로 반환
+			int a = System.in.read();
+			System.out.println(a + ", " + (char)a);
+			
+			//enter 처리
+			System.in.read();// \r  13
+			System.in.read();// \n  10
+			
+			System.out.println("--------------------------");
+			
+			System.out.print("숫자 1개 입력 :");
+			//입력한 숫자를 문자로 인식해서 아스키 코드를 반환하기 떄문에 0을 입력하면 아스키 코드 48을 반환
+			//따라서, 입력한 데이터를 숫자로 바로 사용하고 싶으면 -48을 해야함.
+			int b = System.in.read();
+			System.out.println(b);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
