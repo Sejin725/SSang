e17f7c0566b21e7ea3d313f88b16db37ba0e9a7f
sejin <seajine725@gmail.com>
2023-05-15 16:36:50 +0900

    exchange

--- Study/src/Solution.java
+++ Study/src/Solution.java
@@ -1,24 +1,25 @@
+
 public class Solution {
+	
 
-
-    public int solution(int[] numbers) {
-        int a = numbers[0], b = numbers[0];
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] > a )
-                a = numbers[i];
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] < a && numbers[i] > b)
-                b = numbers[i];
-        }
-        return a*b;
+	public String solution(String my_string, String letter) {
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++) {
+			if (String.valueOf(my_string.charAt(i))!=letter) {
+				answer += my_string.charAt(i);
+			}
+			System.out.println(i + ". " + answer);
+		}
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int a[] = {0, 31, 24, 10, 1, 9};
+        String a1 = "abcdef";
+        String a2 = "f";
+        
         //System.out.println(s.solution("workspace"));
-        System.out.println(s.solution(a));
+        System.out.println(s.solution(a1,a2));
 
     }
 }
